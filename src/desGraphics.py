d25f781dd2d7d07ae145ba435934b63f1c1defd7
Philip A. Wilsey <wilseypa@gmail.com>
2015-01-21 10:26:07 -0500

    cleanup the existing plots and starting to use histogram plotting for some of the data.

--- src/desGraphics.py
+++ src/desGraphics.py
@@ -1,6 +1,7 @@
 import json
 import pylab
 import seaborn
+import numpy as np
 
 from collections import Counter
 
@@ -8,37 +9,41 @@ from collections import Counter
 json_data = open("analysisResults.json")
 data = json.load(json_data)
 
-#--------------------------------------------------------------------------------
-# NOTES:
-#    1. we should probably perform outlier removal to sharpen the presentation.
-
 
 #--------------------------------------------------------------------------------
 # plot the number of events that are available by simulation cycle
 
+pylab.title('Cycle by Cycle Record of the Number of Events Available for Execution')
 pylab.plot(data["events_available_by_sim_cycle"])
-pylab.ylabel('Number of Events Available for Exec')
+pylab.ylabel('Number of Events Available for Exec (log scale to minimize outlier dominance)')
+pylab.yscale('log')
 pylab.xlabel('Simulation Cycle')
 pylab.show()
 
 #--------------------------------------------------------------------------------
-# show the times that X events are available in a simulation cycle
+# plot a histogram of the number of simulation cycles that X events are available
 
-# count the number of time that X events occur
-events_avail = Counter(data["events_available_by_sim_cycle"])
-# put the result into x and y vectors for plotting
-tuple_events = []
-for tmp in events_avail.keys() :
-	tuple_events.append((tmp,events_avail[tmp]))
-x, y = zip(*tuple_events)
+# reject outliers from the mean
+def reject_outliers(data, m=2):
+    return data[abs(data - np.mean(data)) < m * np.std(data)]
 
-pylab.bar(x, y)
-pylab.ylabel('Number of Occurrences')
-pylab.xlabel('Number of Events Available')
+# reject outliers from the median
+def reject_outliers_from_median(data, m = 2.):
+    d = np.abs(data - np.median(data))
+    mdev = np.median(d)
+    s = d/mdev if mdev else 0.
+    return data[s<m]
+
+pylab.title('Number of Simulation Cycles that X Events are Available (outliers removed)')
+pylab.hist(reject_outliers(np.asarray(data["events_available_by_sim_cycle"])))
+#pylab.ylabel('# Simulation Cycles X Events Available')
+pylab.xlabel('Number of Events')
 pylab.show()
 
 #--------------------------------------------------------------------------------
 # plot the percent of local events executed by each LP
+
+# this plot stinks.  we need to find some way to improve it's presentation.
 
 local_events = []
 for tmp in data["lps"] :
@@ -47,8 +52,17 @@ for tmp in data["lps"] :
     percent = round((local / remote) * 100,2)
     local_events.append(percent)
 
-print local_events
-
 pylab.plot(sorted(local_events))
 pylab.ylabel('% of Executed events that were self generated')
+#pylab.ylim(0,100)
+pylab.show()
+
+#--------------------------------------------------------------------------------
+# histogram the percent of local events executed by each LP
+
+pylab.title('Histogram of % of Events that were Self (by the LP) Generated')
+pylab.hist(sorted(local_events))
+pylab.ylabel('Frequency')
+pylab.ylabel('Percent of Executed Events that were Locally Generated')
+#pylab.ylim(0,100)
 pylab.show()
