c3238e80790cafe7615e36a900dcbb65bd29b9e4
crawfordpw <pwc.crawford@gmail.com>
2017-07-05 02:54:25 -0400

    added a skeleton for the desAnalysis refectoring. This is just temporary, as nothing actually works yet. The structure may very well change in the future

---
+++ src/outOfCore/desAnalysis.go
@@ -0,0 +1,8 @@
+// This file is blank right now and is just a temp file. Right now,
+// the idea is to run all of the desAnalysis process from this file, 
+// turning on and off specific analysis that a user may or may not
+// want. From here, it can run the various other files that are created
+// for the analysis process. 
+
+// This may or may get deleted, depending on how this process unfolds
+// in future development. 
