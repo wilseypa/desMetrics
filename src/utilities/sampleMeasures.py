f450d59de3c8b13d416a31bd2fe5510c053c4e3e
Philip A. Wilsey <wilseypa@gmail.com>
2018-02-05 17:40:09 -0500

    revised script to work with different bases in the metric computation.  currently you can compare against the full trace (with the --fulltrace flag) or against the first sample (as determined by a sort of the directories under "sampleDir".

--- src/utilities/sampleMeasures.py
+++ src/utilities/sampleMeasures.py
@@ -1,51 +1,97 @@
 
+## this program is working to compute various metrics on samples extracted from large event trace
+## files for the desMetrics project.  
+
 import os
 import numpy as np
 from scipy.stats import wasserstein_distance
+import matplotlib as mpl
+# Force matplotlib to not use any Xwindows backend
+mpl.use('Agg')
+import palettable
+import pylab
+import argparse
 
-## let's look at events available
+# process the arguments on the command line
+argparser = argparse.ArgumentParser(description='Generate various measures on how well the desAnalysis output results of desSamples files.')
+argparser.add_argument('--fulltrace', 
+                       action="store_true",
+                       help='Compare samples against full trace.')
+args = argparser.parse_args()
 
-# becuase we know there is startup/teardown costs, we'll trim them out at those costs will not be part of the
-# samples; furthermore, we will sort the data as we're really interested in the shape of these curves not the
-# instantaneous variations (which is quite rough if you look at the raw data)
+# create a directory to write output graphs
+measuresDir = 'measuresOutDir/'
+if not os.path.exists(measuresDir):
+    os.makedirs(measuresDir)
 
-data = np.loadtxt("analysisData/eventsAvailableBySimCycle.csv", dtype=np.intc, delimiter = ",", comments="#")
+# set colormap and make it the default
+colors = palettable.colorbrewer.qualitative.Dark2_7.mpl_colors
 
-totalEvents = len(data)
+# define a function to display/save the pylab figures.
+def display_graph(fileName) :
+    print "Creating graphics " + fileName
+    print "    ....writing pdf"
+    pylab.savefig(fileName + ".pdf", bbox_inches='tight')
+    pylab.clf()
+    return
 
-# how many events in 1%
-numSkippedEvents = int(len(data)*.01)
+## ok this is my attempt to generate an analysis procedure that will work with either any trace as the base
+## case; that said, because the full trace can have startup/teardown costs, we'll include a parameter that
+## tells us how many (if any) events to skip at the head/tail of eventsAvailableBySimCycle.csv of the baseDir
+def computeMetrics(baseDir, sampleDirs, skippedEvents):
+    ## let's look at events available
+    baseData = np.loadtxt(baseDir + "/analysisData/eventsAvailableBySimCycle.csv", dtype=np.intc, delimiter = ",", comments="#")
+    totalEvents = len(baseData)
+    percentagesOfBaseData = baseData.astype(float)/float(np.sum(baseData))
 
-print "wasserstein against /analysisData/eventsAvailableBySimCycle.csv"
-print "to sample, distance"
-for x in os.listdir("sampleDir") :
-    approxData = np.loadtxt("sampleDir/" + x + "/analysisData/eventsAvailableBySimCycle.csv", dtype=np.intc,
-                            delimiter = ",", comments="#") 
-    print x + ", %.8f" % wasserstein_distance(
-        sorted(data[numSkippedEvents:totalEvents-numSkippedEvents]),
-        sorted(approxData))
+    print "wasserstein against " + baseDir + "/eventsAvailableBySimCycle.csv"
+    print "To Sample, Wasserstein Distance"
+    for x in sampleDirs :
+        sampleData = np.loadtxt(x + "/analysisData/eventsAvailableBySimCycle.csv", dtype=np.intc, delimiter = ",", comments="#") 
+        print x + ", %.8f" % wasserstein_distance(
+            sorted(baseData[skippedEvents:totalEvents-skippedEvents]),
+            sorted(sampleData))
 
-## now let's look at the summaries of event chains.
+    ## now let's look at the summaries of event chains.
 
-# since we don't want to have the size of the measured data to skew the wasserstein distance results, we'll
-# work from percentages of the populations in question.  furthermore, we don't really want to see the
-# differences by event change length; our real interest is in the impact on the overall event chain
-# computation, we will create a vector that includes measures from local, linked, and global chains as one
-# vector to be analyzed....
+    # since we don't want to have the size of the measured data to skew the wasserstein distance results, 
+    # we'll work from percentages of the populations in question.  furthermore, we don't really want to
+    # see the differences by event change length; our real interest is in the impact on the overall event
+    # chain computation, we will create a vector that includes measures from local, linked, and global
+    # chains as one vector to be analyzed....
     
-print "wasserstein against /analysisData/eventChainsSummary.csv"
-data = np.loadtxt("analysisData/eventChainsSummary.csv", dtype=np.intc, delimiter = ",", comments="#")
-percentagesOfData = data.astype(float)/float(np.sum(data))
+    print "wasserstein against " + baseDir + "/analysisData/eventChainsSummary.csv"
+    baseData = np.loadtxt(baseDir + "/analysisData/eventChainsSummary.csv", dtype=np.intc, delimiter = ",", comments="#")
+    percentagesOfBaseData = baseData.astype(float)/float(np.sum(baseData))
+    
+    print "To Sample, Wasserstein Distance"
+    for x in sampleDirs :
+        sampleData = np.loadtxt(x + "/analysisData/eventChainsSummary.csv", dtype=np.intc,
+                                delimiter = ",", comments="#")
+        percentagesOfSampleData = sampleData.astype(float)/float(np.sum(sampleData))
+        print x + ", %.8f" % wasserstein_distance(
+            np.append(np.append(np.array(percentagesOfBaseData[:,1]),np.array(percentagesOfBaseData[:,2])),np.array(percentagesOfBaseData[:,3])),
+            np.append(np.append(np.array(percentagesOfSampleData[:,1]),np.array(percentagesOfSampleData[:,2])),np.array(percentagesOfSampleData[:,3])))
+            
+    ## now let's look at modularity....
 
-print "to sample, distance"
-for x in os.listdir("sampleDir") :
-    approxData = np.loadtxt("sampleDir/" + x + "/analysisData/eventChainsSummary.csv", dtype=np.intc,
-                            delimiter = ",", comments="#")
-    percentagesOfApproxData = approxData.astype(float)/float(np.sum(approxData))
-    print x + ", %.8f" % wasserstein_distance(
-        np.append(np.append(np.array(percentagesOfData[:,1]),np.array(percentagesOfData[:,2])),np.array(percentagesOfData[:,3])),
-                  np.append(np.append(np.array(percentagesOfApproxData[:,1]),np.array(percentagesOfApproxData[:,2])),np.array(percentagesOfApproxData[:,3])))
+    return
 
+dirs = []
 
-## now let's look at modularity....
+numSkippedEvents = 0
+if args.fulltrace :
+    data = np.loadtxt("analysisData/eventsAvailableBySimCycle.csv", dtype=np.intc, delimiter = ",", comments="#")
+    totalEvents = len(data)
+    # how many events in 1%
+    numSkippedEvents = int(len(data)*.01)
+    dirs.append("./")
 
+for x in sorted(os.listdir("sampleDir")) :
+    dirs.append("sampleDir/" + x)
+              
+#print dirs
+#print dirs[0]
+#print dirs[1:len(dirs)]
+
+computeMetrics(dirs[0],dirs[1:len(dirs)],numSkippedEvents)
