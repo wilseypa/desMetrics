3044902783ad4ca146325bc01d789c9762013b11
Sean Kane <spkane31@gmail.com>
2018-12-02 16:21:24 -0500

    updated desAnalysis

--- src/desAnalysis.go
+++ src/desAnalysis.go
@@ -1,4 +1,3 @@
-
 // this program performs the analysis for the desMetrics project at UC
 // (http://github.com/wilseypa/desMetrics).  this program inputs a json file containing profile data of the
 // simulation model for which the event trace was captured.  the events are stored in a separate (compressed
@@ -346,16 +345,35 @@ func main() {
 	mapIntToLPName := make([]string, numOfLPs)
 	for key, value := range lpNameMap {mapIntToLPName[value.toInt] = key}
 
+	// Now we'll check to see if all LPs sent an event (can be a problem if LP sent one event in first 1 %
+	// and did not receive anything afterwards) will record max num of events
+	// we check this before received events, bc desAnalysis will crash if each LP doesn't send one
+	fmt.Printf("%v: Verifying that all LPs sent at least one event. \n", getTime())
+	maxLPSentArray := 0
+	zeroSentLPS int := 0
+	for i := range lps {
+		if lps[i].sentEvents == 0 {
+			// fmt.Printf("LP %v sent ZERO messages.\n", mapIntToLPName[i])	
+			zeroSentLPS++
+		}
+		if maxLPSentArray < lps[i].sentEvents {maxLPSentArray = lps[i].sentEvents}
+		// if maxLPSentArray < len(lps[i].sentEvents) {maxLPSentArray = len(lps[i].sentEvents)}
+	}
+	fmt.Printf("%v: %v sent ZERO messages.\n", getTime(), zeroSentLPS)
+
 	// let's check to see if all LPs received an event (not necessarily a huge problem, but something we
 	// should probably be aware of.  also, record the max num of events received by an LP
 	fmt.Printf("%v: Verifying that all LPs recieved at least one event.\n", getTime())
 	maxLPEventArray := 0
+	zeroReceivedLPS int := 0
 	for i := range lps {
 		if len(lps[i].events) == 0 {
-			fmt.Printf("WARNING: LP %v recived zero messages.\n", mapIntToLPName[i])
+			// fmt.Printf("WARNING: LP %v recived zero messages.\n", mapIntToLPName[i])
+			zeroReceivedLPS++
 		}
 		if maxLPEventArray < len(lps[i].events) {maxLPEventArray = len(lps[i].events)}
 	}
+	fmt.Printf("%v: %v received ZERO messages.\n", getTime(), , zeroReceivedLPS)
 
 	// we now need to sort the event lists by receive time.  for this we'll use the sort package.
 	fmt.Printf("%v: Sorting the events in each LP by receive time.\n", getTime())
@@ -833,4 +851,4 @@ func main() {
 
 	fmt.Printf("%v: Finished.\n", getTime())
 	return
-}	
+} 
